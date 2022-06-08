cf270cdbb13fa6e7899392a91fc1e7ecc4b1c08b
FatihG34 <fthgnydn34@gmail.com>
2022-06-08 15:45:07 +0300

    finished

--- app/app.js
+++ app/app.js
@@ -1,13 +1,12 @@
 //* variables assigned  here
 const todoText = document.querySelector(".todo_input");
 const addBtn = document.querySelector(".add-btn");
-console.log(todoText);
-console.log(addBtn);
-
 const listField = document.querySelector(".list_filetodo");
 const todoItems = document.querySelector(".todo_items");
-console.log(listField);
-console.log(todoItems);
+//* in here focus on input area
+window.onload = () => {
+    todoText.focus();
+};
 
 addBtn.addEventListener(`click`, () => {
     if (!todoText.value) {
@@ -22,7 +21,7 @@ addBtn.addEventListener(`click`, () => {
         </div>`;
         todoText.value = ``;
     }
-})
+});
 
 todoText.addEventListener("keydown", (e) => {
     if (e.keyCode === 13) {
@@ -30,17 +29,20 @@ todoText.addEventListener("keydown", (e) => {
     }
 });
 
-
 todoItems.addEventListener(`click`, (e) => {
     if (e.target.classList.contains("delete")) {
         e.target.parentElement.remove();
     }
     else if (e.target.classList.contains("todocheck")) {
-        e.target.nextElementSibling.style.textDecoration = "line-through";
+        if (e.target.checked == true) {
+            e.target.nextElementSibling.style.textDecoration = "line-through";
+            e.target.nextElementSibling.style.opacity = "0.4";
+            e.target.style.opacity = "0.4";
+        } else {
+            e.target.nextElementSibling.style.textDecoration = "none";
+            e.target.nextElementSibling.style.opacity = "1";
+            e.target.style.opacity = "1";
+        }
     }
 });
 
-
-window.onload = () => {
-    todoText.focus();
-} 
